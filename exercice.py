33c42a319485e2d84dcd0a426f47db0bcc365c14
HamzaBoukaftane <hamzaboukaftane1996@outlook.fr>
2022-02-13 22:43:09 -0500

    Final update

--- exercice.py
+++ exercice.py
@@ -5,31 +5,35 @@
 import math
 
 def square_root(a: float) -> float:
-    return 0.0
+    return a**(1/2)
 
 
 def square(a: float) -> float:
-    return 0.0
+    return a**2
 
 
 def average(a: float, b: float, c: float) -> float:
-    return 0.0
+    return sum([a,b,c])/3
 
 
 def to_radians(angle_degs: float, angle_mins: float, angle_secs: float) -> float:
-    return 0.0
+    return math.radians(angle_degs+(angle_mins+(angle_secs/60))/60)
 
 
 def to_degrees(angle_rads: float) -> tuple:
-    return 0.0, 0.0, 0.0
+    degrees = math.degrees(angle_rads)
+    min = (degrees - math.floor(degrees)) * 60
+    sec = (min - math.floor(min)) * 60
 
+    return math.floor(degrees), math.floor(min), sec
 
+#Tf = Tc x 1.8 + 32
 def to_celsius(temperature: float) -> float:
-    return 0.0
+    return (temperature-32)/1.8
 
 
 def to_farenheit(temperature: float) -> float:
-    return 0.0
+    return (temperature*1.8)+32
 
 
 def main() -> None:
